0ef2680650aee0789868c14aa5198d5beb2acab5
uynNee <98196142+uynNee@users.noreply.github.com>
2024-05-18 13:39:17 +0700

    Update color_classifier.py
    English translation

--- cys/color_classifier.py
+++ cys/color_classifier.py
@@ -1,65 +1,64 @@
-# 퍼스널 컬러 타입 분류(수정파일)
-# 최초 작성일 : 20/04/23
-# 작성자 : 조예슬
+# Personal Color Type Classification (Modified File)
+# Initial Creation Date: 20/04/23
+# Author: Yesul Cho
 #
-# 작성내용 :
+# Description:
 #
-#         퍼스널컬러 타입 기준점 재조정 후
+#         After readjusting the criteria for personal color types
 
-
-# 함수
-# 기준값에 따라 분류하기
+# Function
+# Classify according to the criteria
 
 # Class
-# 기준값에 따라 분류하기
+# Classify according to the criteria
 
-class Color :
+class Color:
     person_HSV = []
-        
-    def color_classifier(self, person_HSV) :    
+
+    def color_classifier(self, person_HSV):
         self.H = float(person_HSV[0])
         self.S = float(person_HSV[1])
         self.V = float(person_HSV[2])
         diff = round(self.V - self.S, 2)
-    
+
         color_type = ["WSB", "WSL", "WAD", "WAM", "CSL", "CSM", "CWB", "CWD"]
 
-        if self.H >= 23 and self.H <= 203 : 
-            if diff >= 46.25 :
-                if self.S >= 31.00 :
+        if 23 <= self.H <= 203:
+            if diff >= 46.25:
+                if self.S >= 31.00:
                     self.ans = 0
-                    # Warm Spring Bright                            
-                else :
+                    # Warm Spring Bright
+                else:
                     self.ans = 1
                     # Warm Spring Light
 
             elif diff < 46.25:
-                if self.S >= 46.22 :
+                if self.S >= 46.22:
                     self.ans = 2
-                    # Warm Autumn Deep                
-                else :
+                    # Warm Autumn Deep
+                else:
                     self.ans = 3
                     # Warm Autumn Mute
 
-        elif (self.H >= 0 and self.H < 23) or (self.H > 203 and self.H <= 360) :
-            if diff >= 48.75 :
-                if diff >= 28.47 :
+        elif (0 <= self.H < 23) or (203 < self.H <= 360):
+            if diff >= 48.75:
+                if diff >= 28.47:
                     self.ans = 4
-                    # Cool Summer Light                
-                else :
+                    # Cool Summer Light
+                else:
                     self.ans = 5
                     # Cool Summer Mute
 
             elif diff < 48.75:
-                if diff >= 31.26 :
+                if diff >= 31.26:
                     self.ans = 6
-                    # Cool Winter Bright                
-                else :
+                    # Cool Winter Bright
+                else:
                     self.ans = 7
                     # Cool Winter Deep
 
-        else :
+        else:
             self.ans = -1
-            # 에러
-            
+            # Error
+
         return self.ans
